4de925e526597099e772b213bfd7b2a4cff1fc63
Sojeong Lee <sj27419@gmail.com>
2026-07-04 22:49:38 +0900

    feat: NPE

---
+++ src/ref/BigData.java
@@ -0,0 +1,6 @@
+package ref;
+
+public class BigData {
+    Data data; //null 참조형이니까
+    int count; //0
+}
---
+++ src/ref/NullMain4.java
@@ -0,0 +1,15 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData();
+        bigData.data = new Data();
+
+        System.out.println("bigData.count = "+ bigData.count);
+        System.out.println("bigData.data = "+ bigData.data);
+
+        // npe
+        System.out.println("bigData.count = "+ bigData.data.value);
+
+    }
+}
