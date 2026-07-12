37c29a546f5be911c4c6b3cb764a16c1a1476715
Sojeong Lee <sj27419@gmail.com>
2026-07-12 23:38:30 +0900

    feat: 접근 제어자 이해 1

---
+++ src/com/access/SpeakerMain.java
@@ -0,0 +1,23 @@
+package com.access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+
+        speaker.volumeUp(); // 볼륨 증가 더이상 안됨!
+        speaker.volumeUp();
+        speaker.volumeUp();
+        speaker.volumeDown();
+        speaker.showVolume();
+
+        //필드에 직접 접근
+        System.out.println("볼륨 필드에 직접 접근해서 수정");
+        speaker.volume=200;
+        speaker.showVolume();
+    }
+}
