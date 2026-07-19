a560cfa0fdfbdf28344c4fbd4136664888e02048
Sojeong Lee <sj27419@gmail.com>
2026-07-19 23:47:54 +0900

    feat: 접근 제어자 - 문제2

---
+++ src/com/access/ex/ShoppingCart.java
@@ -0,0 +1,39 @@
+package com.access.ex;
+
+
+public class ShoppingCart {
+    private Item[] items = new Item[10];
+    private int itemCount;
+
+    public void addItem(Item item){
+        if (itemCount >= items.length) {
+            System.out.println("크기 초과");
+            return;
+        }
+        items[itemCount] = item;
+        itemCount++;
+    }
+
+    public void displayItems(){
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            System.out.println("상품명:" + item.getName() + ", 합계:" + item.getTotalPrice());
+        }
+        System.out.println("전체 가격 합:" + calculateTotalPrice());
+    }
+
+    public void displayItems(Item item){
+        System.out.println(item.getName());
+        System.out.println(item.getPrice());
+        System.out.println(item.getQuantity());
+    }
+
+    private int calculateTotalPrice(){
+        int totalPrice = 0;
+        for (int i=0; i<itemCount; i++){
+            Item item = items[i];
+            totalPrice += item.getTotalPrice();
+        }
+        return totalPrice;
+    }
+}
