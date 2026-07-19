5ae57eb483714ac025321b4035f5f98a6ed6ce39
Sojeong Lee <sj27419@gmail.com>
2026-07-19 23:10:51 +0900

    feat: 접근 제어자 - 얘시

---
+++ src/com/access/a/BankAccount.java
@@ -0,0 +1,36 @@
+package com.access.a;
+
+public class BankAccount {
+
+    private int balance;
+
+    public BankAccount() {
+        balance = 0;
+    }
+
+    // public method : deposit
+    public void deposit(int amount){
+        if (isAmountValid(amount)){
+            balance += amount;
+        }else {
+            System.out.println("유효하지 않은 금액");
+        }
+    }
+
+    public void withdraw(int amount){
+        if (isAmountValid(amount) && balance - amount >= 0){
+            balance -= amount;
+        }else {
+            System.out.println("유효하지 않은 금액이거나 잔액이 부족합니다.");
+        }
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    private boolean isAmountValid(int amount){ // 이 함수만 private!
+        // 금액이 0보다 커야 함
+        return amount >0;
+    }
+}
