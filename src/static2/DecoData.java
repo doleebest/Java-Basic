0abd2c0ca8427f4db755f403ca91575e51053025
Sojeong Lee <sj27419@gmail.com>
2026-08-17 21:45:56 +0900

    feature: static, instance - 아자아자

---
+++ src/static2/DecoData.java
@@ -0,0 +1,29 @@
+package static2;
+
+public class DecoData {
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall(){
+
+        // compile error
+//        instanceValue++; // 인스턴스 변수 접근
+//        instanceMethod()++; // 인스턴스 메서드 접근
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    private void instanceMethod(){ // 모든 곳에 다 접근 가능
+        instanceValue++;
+        instanceMethod();
+
+        staticMethod();
+        staticValue++;
+        System.out.println("instanceValue= "+ instanceValue);
+    }
+
+    private static void staticMethod(){
+        System.out.println("staticValue= "+ staticValue);
+    }
+}
