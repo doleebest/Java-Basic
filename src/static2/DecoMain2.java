1e9f0a30d84aa9a9c26e12e3ce778d846ee26331
Sojeong Lee <sj27419@gmail.com>
2026-08-17 15:05:27 +0900

    feature: static deco()

--- src/static2/DecoMain2.java
+++ src/static2/DecoMain2.java
@@ -1,10 +1,9 @@
 package static2;
 
-public class DecoMain1 {
+public class DecoMain2 {
     public static void main(String[] args) {
         String s = "hello java";
-        DecoUtil1 utils = new DecoUtil1(); // 이 객체을 또 생성하는 것은 의미가 없다.
-        String deco = utils.deco(s);
+        String deco = DecoUtil2.deco(s);
 
         System.out.println("before "+s);
         System.out.println("after "+deco);
---
+++ src/static2/DecoUtil2.java
@@ -0,0 +1,7 @@
+package static2;
+
+public class DecoUtil2 {
+    public static String deco(String str){ // 붕어빵 틀에 바로 존재하는 메서드
+        return "*" + str + "*";
+    }
+}
