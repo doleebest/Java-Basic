42240cd629b7e5bd6adb3de6745e9a83a766f202
Sojeong Lee <sj27419@gmail.com>
2026-08-17 14:25:55 +0900

    feature: static 변수 1

---
+++ src/memory/Data1.java
@@ -0,0 +1,11 @@
+package memory;
+
+public class Data1 {
+    public String name;
+    public int count;
+
+    public Data1(String name){ // 생성된 객체의 수 세기
+        this.name = name;
+        count++;
+    }
+}
