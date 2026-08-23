6912c0bc41a21831e61a093fc5adbfaff0f420ef
Sojeong Lee <sj27419@gmail.com>
2026-08-23 21:23:49 +0900

    feature: static final은 생성자로 변경 불가

---
+++ src/final1/Fieldinit.java
@@ -0,0 +1,12 @@
+package final1;
+
+public class Fieldinit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+//    public Fieldinit(int value){
+//        this.value = value;
+//    }
+
+}
